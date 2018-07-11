09dd490c116578c6975826a8ecce73aa1b610cbe
Samir Carvalho <samircarvalho@macbook-pro-de-samir.local>
2018-07-11 00:10:41 -0300

    003. TableView and Extensions included.

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tableView: UITableView = {
+        let tableView = UITableView(frame: UIScreen.main.bounds)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        self.tableView.dataSource = self
+        self.view.addSubview(tableView)
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test GitHub"
+        return cell
+    }
+}

7cb1474058c5b4d52da957c8eb65ecde326378d8
Samir Carvalho <samircarvalho@macbook-pro-de-samir.local>
2018-07-11 23:29:02 -0300

    007. Prepare to change the ViewController by a TableViewController, Added a JSON file and a GitHub Model Struct.

---
+++ TableView/GitHubExample.swift
@@ -0,0 +1,14 @@
+//
+//  GitHubExample.swift
+//  TableView
+//
+//  Created by Samir Carvalho on 11/07/2018.
+//  Copyright © 2018 KDEA Solutions. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubExample {
+    let title: String
+    let status: String
+}
---
+++ TableView/GitHubTestTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GitHubTestTableViewController.swift
+//  TableView
+//
+//  Created by Samir Carvalho on 11/07/2018.
+//  Copyright © 2018 KDEA Solutions. All rights reserved.
+//
+
+import UIKit
+
+class GitHubTestTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}
